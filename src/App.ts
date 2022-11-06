0eafb288c5823971b8ce8c07646a2243f492e456
simon44556 <simon44556@gmail.com>
2022-11-06 20:35:25 +0100

    - Remove index

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,4 @@
-import { Engine, Light, Mesh, MeshBuilder, Scene, TargetCamera, Vector3 } from "@babylonjs/core";
+import { Color3, Engine, Light, Mesh, MeshBuilder, Scene, TargetCamera, Vector3 } from "@babylonjs/core";
 import "@babylonjs/inspector";
 
 import { KeyboardEvents } from "./Input/KeyboardEvents";
@@ -28,21 +28,45 @@ class App {
     this._scene.debugLayer.show();
 
     this._lightBuilder = new LightBuilder(this._scene);
-
-    const light: Light = this._lightBuilder.buildHemisphericLight("MainLight", new Vector3(1, 20, 10));
-
-    const mesh: Mesh = MeshBuilder.CreateBox("MyBox", { size: 5 }, this._scene);
-
-    const material = new SampleMaterial("groundMat", this._scene);
-    const ground = MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, this._scene);
-    ground.material = material;
-    ground.position.y = -4;
-
     this._camera = new TargetCameraHandler(this._scene).buildArcCamera("cam", Math.PI / 2, Math.PI / 3, 10, new Vector3(0, 5, 10)).attachControls(this._canvas);
-
     this._keyboardEvents = new KeyboardEvents(this._scene);
 
+    //TODO: Move this out of main
+    this.addLight();
+    this.addBox();
+    this.addGround();
+
     this.renderLoop();
+  }
+
+  addLight() {
+    const light: Light = this._lightBuilder.buildHemisphericLight("MainLight", new Vector3(0, 10, 10));
+    light.diffuse = new Color3(0.01, 0.0, 0.5);
+  }
+
+  addBox() {
+    const mesh: Mesh = MeshBuilder.CreateBox("MyBox", { size: 1 }, this._scene);
+    mesh.material = new SampleMaterial("otherMat", this._scene);
+    mesh.isVisible = false;
+
+    const meshArray: Mesh[] = [];
+
+    for (let x = 0; x < 5; x++) {
+      for (let y = 0; y < 5; y++) {
+        for (let z = 0; z < 5; z++) {
+          meshArray.push(mesh.clone("anotherCube" + x + y + z));
+          meshArray[meshArray.length - 1].isVisible = true;
+          meshArray[meshArray.length - 1].position = new Vector3(x, y, z);
+        }
+      }
+    }
+  }
+
+  addGround() {
+    const material: SampleMaterial = new SampleMaterial("groundMat", this._scene);
+    const ground: Mesh = MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, this._scene);
+    ground.material = material;
+    ground.position.y = -4;
   }
 
   async renderLoop() {
